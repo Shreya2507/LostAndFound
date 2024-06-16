af3ec6d04e9479f7a31b8608ea2b1c8938e25577
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-06-16 16:50:08 +0530

    header

--- LF/src/App.jsx
+++ LF/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css'
+import Header from './Components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  
+  <div>
+    <Router>
+        <Header />
+        <div>
+          <Switch>
+            <Route path="/Notes">
+              <Notes />
+            </Route>
+            <Route path="/Doubt">
+              <Doubt />
+            </Route>
+            <Route path="/PYQ">
+              <PYQ />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  </div>
 }
 
 export default App
--- LF/src/Components/Header.jsx
+++ LF/src/Components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ncu from '../Assets/ncu.png';
+import '../App.css';
+import './header.css';
+
 
 function Header() {
   return (
     <div>
-      
+      <header className="page-header">
+        <Link to="/">
+          <div className="logo">
+          <Link to="/App"><img alt="logo" src={ncu} className="logo-img" /></Link>
+          </div>
+        </Link>
+        <div className="head">
+          <nav className="navbar">
+            <Link to="/">HOME</Link>
+            <Link to="/">LOST ITEMS</Link>
+            <Link to="/">FOUND ITEMS</Link>
+            <Link to="/">REPORT</Link>
+          </nav>
+
+        </div>
+      </header>
     </div>
   )
 }
