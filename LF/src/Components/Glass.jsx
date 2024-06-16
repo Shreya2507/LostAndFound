26b4b87e1a76ef3e76ad95a49364bfb90e8a1df0
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-06-16 18:45:14 +0530

    glass

--- LF/src/Components/Glass.jsx
+++ LF/src/Components/Glass.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './glass.css';
 
-function Glass() {
+function Glass({ title, subtitle, imageSrc }) {
   return (
-    <div>
-      
+    <div className="cardContainer">
+      <div className="card">
+        <p className="city">{title}</p>
+        <p className="weather">{subtitle}</p>
+        <img
+          width="100"
+          height="100"
+          src={imageSrc}
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Glass
+export default Glass;
--- LF/src/Pages/Home.jsx
+++ LF/src/Pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Glass from '../Components/Glass';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home">
+      <Glass 
+        title="Report a Lost Item"
+        subtitle="Report"
+        imageSrc="path/to/lost-item-image.png"
+      />
+      <Glass 
+        title="Found an Item"
+        subtitle="Report"
+        imageSrc="path/to/found-item-image.png"
+      />
+      <Glass 
+        title="Find Your Lost Item"
+        subtitle="Browse"
+        imageSrc="path/to/view-lost-items-image.png"
+      />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
