31dd83200dcac4381123a1dd37f07451441bd2a8
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-06-17 19:34:00 +0530

    background

--- LF/src/Pages/Home.jsx
+++ LF/src/Pages/Home.jsx
@@ -4,34 +4,43 @@ import './Home.css';
 import lossImage from '../Assets/loss.png';
 import foundImage from '../Assets/found.png';
 import findImage from '../Assets/find.png';
-import LF from '../Assets/LF.jpg';
 
 function Home() {
   return (
-<>
-    <div className="homepage">
-
-    </div>
-    <div className="home">
-      <Glass 
-        title="Report a Lost Item"
-        subtitle="Report"
-        imageSrc={lossImage}
-        to="/Report"
-      />
-      <Glass 
-        title="Found an Item"
-        subtitle="Report"
-        imageSrc={foundImage}
-        to="/Founditm"
-      />
-      <Glass 
-        title="Find Your Lost Item"
-        subtitle="Browse"
-        imageSrc={findImage}
-        to="/Lostitm"
-      />
-    </div>
+    <>
+      <div className="homepage">
+        <div className="content1">
+          <h1 className="title1">
+            Lost & Found
+            <div className="aurora">
+              <div className="aurora__item"></div>
+              <div className="aurora__item"></div>
+              <div className="aurora__item"></div>
+              <div className="aurora__item"></div>
+            </div>
+          </h1>
+        </div>
+      </div>
+      <div className="home">
+        <Glass 
+          title="Report a Lost Item"
+          subtitle="Report"
+          imageSrc={lossImage}
+          to="/Report"
+        />
+        <Glass 
+          title="Found an Item"
+          subtitle="Report"
+          imageSrc={foundImage}
+          to="/Founditm"
+        />
+        <Glass 
+          title="Find Your Lost Item"
+          subtitle="Browse"
+          imageSrc={findImage}
+          to="/Lostitm"
+        />
+      </div>
     </>
   );
 }
