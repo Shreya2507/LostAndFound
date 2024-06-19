69656ad3a140d5b71ffa345a12faa2745579b40c
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-06-20 00:20:34 +0530

    footerdone

--- LF/src/Components/Glass.jsx
+++ LF/src/Components/Glass.jsx
@@ -1,26 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import VanillaTilt from 'vanilla-tilt';
 import './glass.css';
+import '../App.css';
 
 function Glass({ title, subtitle, imageSrc, to }) {
   const cardRef = useRef(null);
 
   useEffect(() => {
     const card = cardRef.current;
-    
+
     // Initialize VanillaTilt
     VanillaTilt.init(card, {
       max: 15,
       speed: 400,
       glare: true,
-      "max-glare": 0.2,
+      'max-glare': 0.2,
     });
 
     // Mousemove effect
     const handleMouseMove = (e) => {
-      let x = e.pageX - (window.innerWidth / 2);
-      let y = e.pageY - (window.innerHeight / 2);
+      let x = e.pageX - window.innerWidth / 2;
+      let y = e.pageY - window.innerHeight / 2;
       card.style.transform = `translate(${x / 40}px, ${y / 40}px)`;
     };
 
@@ -33,30 +37,26 @@ function Glass({ title, subtitle, imageSrc, to }) {
 
   return (
     <>
-    <div className="cardContainer">
-      <Link to={to} className="cardLink">
-        <div ref={cardRef} className="card">
-          <div className="text-card">
-            <h1 className="content">{title}</h1>
-            <h3 className="content">{subtitle}</h3>
-          </div>
-          
-          <img
-            width="100"
-            height="100"
-            src={imageSrc}
-            alt={title}
-          />
-          
-        </div>
-      </Link>
-    </div>
-    <div className="balls">
-            <div className="ball ball1"></div>
-            <div className="ball ball2"></div>
-            <div className="ball ball3"></div> 
+      <div className="glass-container">
+        <div className="cardContainer">
+          <Link to={to} className="cardLink">
+            <div ref={cardRef} className="card">
+              <div className="text-card">
+                <h1 className="content">{title}</h1>
+                <h3 className="content">{subtitle}</h3>
+              </div>
+
+              <img width="100" height="100" src={imageSrc} alt={title} />
             </div>
-          </>
+          </Link>
+        </div>
+        <div className="balls">
+          <div className="ball ball1"></div>
+          <div className="ball ball2"></div>
+          <div className="ball ball3"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
