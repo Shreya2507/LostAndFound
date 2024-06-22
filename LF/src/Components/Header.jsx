643aeb62e006825ec63d7ff3f0877da433f83b01
SohamV8 <sohamvashists8@gmail.com>
2024-06-22 13:31:21 +0530

    darkmode

--- LF/src/Components/Header.jsx
+++ LF/src/Components/Header.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ncu from '../Assets/ncu.png';
+import ncuDark from '../Assets/ncuDark.png'; // Import the dark mode logo
 import '../App.css';
 import './header.css';
 import ToggleSwitch from './ToggleSwitch';
 
 function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const bodyClass = document.body.classList;
+    if (isDarkMode) {
+      bodyClass.add('dark');
+    } else {
+      bodyClass.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div>
       <header className="page-header">
         <div className="logo">
           <Link to="/Home">
-            <img alt="logo" src={ncu} className="logo-img" />
+            <img alt="logo" src={isDarkMode ? ncuDark : ncu} className="logo-img" />
           </Link>
         </div>
         <div className="head">
@@ -22,7 +38,7 @@ function Header() {
             <Link to="/Report">REPORT</Link>
           </nav>
         </div>
-        <ToggleSwitch />
+        <ToggleSwitch onToggle={handleToggle} />
         <div className="btns">
           <Link className="blue-btn" to="/Profile">
             <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/gender-neutral-user.png" alt="Profile" />
