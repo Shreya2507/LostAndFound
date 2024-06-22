1117dc89b342437959852a90a4560429ac7672df
SohamV8 <sohamvashists8@gmail.com>
2024-06-22 13:48:45 +0530

    darkmode

--- LF/src/Components/Report.jsx
+++ LF/src/Components/Report.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import React, { useState } from "react";
 import './ReportForm.css';
 
 export default function ReportForm() {
@@ -26,95 +26,96 @@ export default function ReportForm() {
 
   return (
     <div className="Form">
-    <div className="container1">
-      <div className="form-wrapper">
-        <h1 className="form-title">REPORT A LOST ITEM</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleReport();
-          }}
-          className="form"
-        >
-          <input
-            type="text"
-            value={itemName}
-            placeholder="Item Name"
-            id="itemName"
-            onChange={(e) => setItemName(e.target.value)}
-            className="input"
-          />
-          <select
-            id="location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            className="input"
+      <div className="container1 dark-mode">
+        <div className="form-wrapper">
+          <h1 className="form-title">REPORT A LOST ITEM</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleReport();
+            }}
+            className="form"
           >
-            <option value={location} disabled>
-              Location
-            </option>
-            <option value="sportRoom">Sport Room</option>
-            <option value="cricketGround">Cricket Ground</option>
-            <option value="cafe10">Cafe 10</option>
-            <option value="cafe1">Cafe 1</option>
-            <option value="cadLab">Cad Lab</option>
-          </select>
-          <select
-            id="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="input"
-          >
-            <option value={category} disabled>
-              Category
-            </option>
-            <option value="electronic">Electronic</option>
-            <option value="notebook">Stationery</option>
-            <option value="mobile">Mobile</option>
-            <option value="laptop">Laptop</option>
-            <option value="sport">Sport</option>
-          </select>
-          <input
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            type="date"
-            id="date"
-            className="input"
-          />
-          <input
-            type="file"
-            id="file"
-            multiple
-            onSubmit={handleImage}
-            className="input"
-          />
-          <textarea
-            placeholder={desc}
-            onChange={(e) => setDesc(e.target.value)}
-            id="description"
-            rows="5"
-            className="textarea"
-          ></textarea>
-          <div className="button-group">
             <input
-              type="submit"
-              onClick={handleReport}
-              className="button21 submit-button"
+              type="text"
+              value={itemName}
+              placeholder="Item Name"
+              id="itemName"
+              onChange={(e) => setItemName(e.target.value)}
+              className="input"
             />
-             <input
-              type="button"
-              value="Cancel"
-              onClick={clearForm}
-              className="button21 cancel-button"
+            <select
+              id="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              className="input"
+            >
+              <option value={location} disabled>
+                Location
+              </option>
+              <option value="sportRoom">Sport Room</option>
+              <option value="cricketGround">Cricket Ground</option>
+              <option value="cafe10">Cafe 10</option>
+              <option value="cafe1">Cafe 1</option>
+              <option value="cadLab">Cad Lab</option>
+            </select>
+            <select
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="input"
+            >
+              <option value={category} disabled>
+                Category
+              </option>
+              <option value="electronic">Electronic</option>
+              <option value="notebook">Stationery</option>
+              <option value="mobile">Mobile</option>
+              <option value="laptop">Laptop</option>
+              <option value="sport">Sport</option>
+            </select>
+            <input
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+              id="date"
+              className="input"
             />
-          </div>
-        </form>
-      </div>
-      <div className="balls21">
+            <input
+              type="file"
+              id="file"
+              multiple
+              onSubmit={handleImage}
+              className="input"
+            />
+            <textarea
+              placeholder={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              id="description"
+              rows="5"
+              className="textarea"
+            ></textarea>
+            <div className="button-group">
+              <input
+                type="submit"
+                onClick={handleReport}
+                className="button21 submit-button"
+                value="Submit"
+              />
+              <input
+                type="button"
+                value="Cancel"
+                onClick={clearForm}
+                className="button21 cancel-button"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="balls21">
           <div className="ball21 ballz1"></div>
           <div className="ball21 ballz2"></div>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
