4aa0b154e77fc7f5a6f646483241deb5c1ada214
Shreya Mathur <smathur245@gmail.com>
2024-06-28 18:07:29 +0530

    minor changes

--- LF/src/Components/Report.jsx
+++ LF/src/Components/Report.jsx
@@ -43,7 +43,7 @@ export default function ReportForm() {
       images
     };
 
-    const endpoint = reportType === "lost" ? 'lost-backend-ten.vercel.app/api/reports/lost' : 'lost-backend-ten.vercel.app/api/reports/found';
+    const endpoint = reportType === "lost" ? 'https://lost-and-found-ncu.vercel.app/api/reports/lost' : 'https://lost-and-found-ncu.vercel.app/api/reports/found';
 
     try {
       await axios.post(endpoint, report);
