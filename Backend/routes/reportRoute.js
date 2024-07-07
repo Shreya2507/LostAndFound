908f32d9696dc2e12623d054be2a1931b9076b70
Shreya Mathur <smathur245@gmail.com>
2024-07-08 00:43:30 +0530

    added routes

--- Backend/routes/reportRoute.js
+++ Backend/routes/reportRoute.js
@@ -23,4 +23,56 @@ router.post('/reports/found', async (req, res) => {
     }
 });
 
+// GET ALL 'LOST' ITEMS
+router.get('/lost-items', async (req, res) => {
+    try {
+        const lostItems = await Lost.find({ status: false }); //ITEMS THAT HAVE NOT BEEN CLAIMED
+
+        if (lostItems.length === 0) {
+            return res.status(404).json({ message: 'No lost items found' });
+        }
+
+        res.status(200).json(lostItems);
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error });
+    }
+});
+
+// GET ALL 'FOUND' ITEMS
+router.get('/found-items', async (req, res) => {
+    try {
+        const foundItems = await Found.find({ status: false }); //ITEMS THAT HAVE NOT BEEN CLAIMED
+
+        if (foundItems.length === 0) {
+            return res.status(404).json({ message: 'No items found' });
+        }
+
+        res.status(200).json(foundItems);
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error });
+    }
+});
+
+
+//CLAIMED ITEM
+router.put('/update-status', async (req, res) => {
+    const { email, reportId } = req.body;
+
+    try {
+        const report = await Report.findOneAndUpdate(
+            { _id: reportId, email: email },
+            { $set: { status: true } },
+            { new: true }
+        );
+
+        if (!report) {
+            return res.status(404).json({ message: 'Report not found' });
+        }
+
+        res.status(200).json({ message: 'Status updated successfully', report });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occurred', error });
+    }
+});
+
 module.exports = reportRoute;
