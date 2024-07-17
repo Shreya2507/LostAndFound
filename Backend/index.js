aab4aae2651aef0d44f927e74753a268a5950b37
YashSharma10 <yashsharma67953@gmail.com>
2024-07-18 00:58:24 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./connection'); // Adjust the path if needed
-const { Lost, Found } = require('./models/Report'); // Adjust the path as needed
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from './app.js'
+dotenv.config({
+  path: './.env'
+})
 
-require('dotenv').config();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  })
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-connectDB();
-
-app.post('/api/reports/lost', async (req, res) => {
-  try {
-    const lostReport = new Lost(req.body);
-    await lostReport.save();
-    res.status(201).send(lostReport);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.post('/api/reports/found', async (req, res) => {
-  try {
-    const foundReport = new Found(req.body);
-    await foundReport.save();
-    res.status(201).send(foundReport);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
