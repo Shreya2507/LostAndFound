14490aa3d907b515d042055a74890f13bdbc8b07
Soham Vashist <113818944+SohamV8@users.noreply.github.com>
2024-07-18 15:50:22 +0530

    try

--- LF/src/Components/Login.jsx
+++ LF/src/Components/Login.jsx
@@ -16,44 +16,44 @@ const Login = () => {
     <div className="Login-Form">
     <div className={`container-log ${isRightPanelActive ? 'right-panel-active' : ''}`} id="container-log">
       <div className="form-container-log sign-up-container-log">
-        <form action="#">
-          <h1>Create Account</h1>
+        <form className='logform' action="#">
+          <h1 className='head1'>Create Account</h1>
           <div className="social-container-log">
-            <a href="#" className="social-log"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social-log"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social-log"><i className="fab fa-linkedin-in"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-facebook-f"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-google-plus-g"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-linkedin-in"></i></a>
           </div>
-          <span>or use your email for registration</span>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <span className='span'>or use your email for registration</span>
+          <input className='loginput' type="text" placeholder="Name" />
+          <input className='loginput' type="email" placeholder="Email" />
+          <input className='loginput' type="password" placeholder="Password" />
           <button className='log'>Sign Up</button>
         </form>
       </div>
       <div className="form-container-log sign-in-container-log">
-        <form action="#">
-          <h1>Sign in</h1>
+        <form className='logform' action="#">
+          <h1 className='head1'>Sign in</h1>
           <div className="social-container-log">
-            <a href="#" className="social-log"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social-log"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social-log"><i className="fab fa-linkedin-in"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-facebook-f"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-google-plus-g"></i></a>
+            <a href="#" className="social-log links"><i className="fab fa-linkedin-in"></i></a>
           </div>
-          <span>or use your account</span>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="#">Forgot your password?</a>
+          <span className='span'>or use your account</span>
+          <input className='loginput' type="email" placeholder="Email" />
+          <input className='loginput' type="password" placeholder="Password" />
+          <a href="#" className='links'>Forgot your password?</a>
           <button className='log'>Sign In</button>
         </form>
       </div>
       <div className="overlay-container-log">
         <div className="overlay-log">
           <div className="overlay-panel-log overlay-left-log">
-            <h1>Welcome Back!</h1>
+            <h1 className='head1'>Welcome Back!</h1>
             <p className='login'>To keep connected with us please login with your personal info</p>
             <button className="ghost-log" id="signIn" onClick={handleSignInClick}>Sign In</button>
           </div>
           <div className="overlay-panel-log overlay-right-log">
-            <h1>Hello, Friend!</h1>
+            <h1 className='head1'>Hello, Friend!</h1>
             <p className='login'>Enter your personal details and start journey with us</p>
             <button className="ghost-log" id="signUp" onClick={handleSignUpClick}>Sign Up</button>
           </div>
